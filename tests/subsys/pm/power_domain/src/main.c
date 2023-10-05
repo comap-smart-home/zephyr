comap-smart-home/zephyr

commit 2ef95ecd8fdbc102ff414e1da14369554911c775

pm: device_runtime: balanced power domain operations
Only run `pm_device_runtime_put` on the dependent domain if the original
claim operation succeeded. This fixes unbalanced operations when running
```
pm_device_runtime_get(dev);
pm_device_runtime_put(dev);
```
On a device that does not have PM enabled but does have a power domain.

Signed-off-by: Jordan Yates <jordan.yates@data61.csiro.au>

include/zephyr/pm/device.h
@@ -32,6 +32,8 @@ enum pm_device_flag {
 	PM_DEVICE_FLAG_BUSY,
 	/** Indicate if the device failed to power up. */
 	PM_DEVICE_FLAG_TURN_ON_FAILED,
+	/** Indicate if the device has claimed a power domain */
+	PM_DEVICE_FLAG_PD_CLAIMED,
 	/**
 	 * Indicates whether or not the device is capable of waking the system
 	 * up.

subsys/pm/device_runtime.c
@@ -163,6 +163,8 @@ int pm_device_runtime_get(const struct device *dev)
 			ret = -EAGAIN;
 			goto unlock;
 		}
+		/* Power domain successfully claimed */
+		atomic_set_bit(&pm->flags, PM_DEVICE_FLAG_PD_CLAIMED);
 	}
 
 	pm->usage++;
@@ -216,7 +218,8 @@ int pm_device_runtime_put(const struct device *dev)
 	/*
 	 * Now put the domain
 	 */
-	if ((ret == 0) && PM_DOMAIN(dev->pm) != NULL) {
+	if ((ret == 0) &&
+	    atomic_test_and_clear_bit(&dev->pm->flags, PM_DEVICE_FLAG_PD_CLAIMED)) {
 		ret = pm_device_runtime_put(PM_DOMAIN(dev->pm));
 	}
 	SYS_PORT_TRACING_FUNC_EXIT(pm, device_runtime_put, dev, ret);

commit beb3532f8f9fea22e3e0fd5f0943e9dc1eac188a

Fix power domain shutdown.
Correct a problem introduced by 2ef95ecd8fdbc102ff414e1da14369554911c775.

include/zephyr/pm/device.h
@@ -33,8 +33,6 @@ enum pm_device_flag {
 	PM_DEVICE_FLAG_BUSY,
 	/** Indicate if the device failed to power up. */
 	PM_DEVICE_FLAG_TURN_ON_FAILED,
-	/** Indicate if the device has claimed a power domain */
-	PM_DEVICE_FLAG_PD_CLAIMED,
 	/**
 	 * Indicates whether or not the device is capable of waking the system
 	 * up.

subsys/pm/device_runtime.c
@@ -163,8 +163,6 @@ int pm_device_runtime_get(const struct device *dev)
 			ret = -EAGAIN;
 			goto unlock;
 		}
-		/* Power domain successfully claimed */
-		atomic_set_bit(&pm->flags, PM_DEVICE_FLAG_PD_CLAIMED);
 	}
 
 	pm->usage++;
@@ -213,13 +211,20 @@ int pm_device_runtime_put(const struct device *dev)
 	}
 
 	SYS_PORT_TRACING_FUNC_ENTER(pm, device_runtime_put, dev);
+
+	/*
+	 * Early return if device runtime is not enabled.
+	 */
+	if (!atomic_test_bit(&dev->pm->flags, PM_DEVICE_FLAG_RUNTIME_ENABLED)) {
+		return 0;
+	}
+
 	ret = runtime_suspend(dev, false);
 
 	/*
 	 * Now put the domain
 	 */
-	if ((ret == 0) &&
-	    atomic_test_and_clear_bit(&dev->pm->flags, PM_DEVICE_FLAG_PD_CLAIMED)) {
+	if ((ret == 0) && PM_DOMAIN(dev->pm)) {
 		ret = pm_device_runtime_put(PM_DOMAIN(dev->pm));
 	}
 	SYS_PORT_TRACING_FUNC_EXIT(pm, device_runtime_put, dev, ret);

tests/subsys/pm/power_domain/app.overlay
@@ -32,4 +32,15 @@
 		status = "okay";
 		power-domain = <&test_domain_balanced>;
 	};
+
+	test_domain_multiple_call: test_domain_multiple_call {
+		compatible = "power-domain";
+		status = "okay";
+	};
+
+	test_dev_multiple_call: test_dev_multiple_call {
+		compatible = "test-device-pm";
+		status = "okay";
+		power-domain = <&test_domain_multiple_call>;
+	};
 };

tests/subsys/pm/power_domain/prj.conf
@@ -6,3 +6,5 @@ CONFIG_PM_DEVICE_POWER_DOMAIN_DYNAMIC=y
 CONFIG_PM_DEVICE_RUNTIME=y
 CONFIG_MP_MAX_NUM_CPUS=1
 CONFIG_ZTEST_NEW_API=y
+
+CONFIG_DEBUG=y

tests/subsys/pm/power_domain/src/main.c
@@ -280,5 +280,68 @@ ZTEST(power_domain_1cpu, test_power_domain_device_balanced)
 	zassert_equal(state, PM_DEVICE_STATE_ACTIVE);
 }
 
+#define TEST_DOMAIN_MULTIPLE_CALL DT_NODELABEL(test_domain_multiple_call)
+#define TEST_DEV_MULTIPLE_CALL DT_NODELABEL(test_dev_multiple_call)
+
+PM_DEVICE_DT_DEFINE(TEST_DOMAIN_MULTIPLE_CALL, domain_pm_action);
+DEVICE_DT_DEFINE(TEST_DOMAIN_MULTIPLE_CALL, NULL, PM_DEVICE_DT_GET(TEST_DOMAIN_MULTIPLE_CALL),
+		 NULL, NULL, POST_KERNEL, 10, NULL);
+
+PM_DEVICE_DT_DEFINE(TEST_DEV_MULTIPLE_CALL, deva_pm_action);
+DEVICE_DT_DEFINE(TEST_DEV_MULTIPLE_CALL, NULL, PM_DEVICE_DT_GET(TEST_DEV_MULTIPLE_CALL),
+		 NULL, NULL, POST_KERNEL, 20, NULL);
+
+/**
+ * @brief Test power domain multiple consecutive call to PM device runtime
+ *
+ * Scenarios tested:
+ *
+ * - get - get + put + put device with a PD while PM is enabled
+ */
+ZTEST(power_domain_1cpu, test_power_domain_multiple_consecutive_call_to_pm_device_runtime)
+{
+	const struct device *domain = DEVICE_DT_GET(TEST_DOMAIN_MULTIPLE_CALL);
+	const struct device *dev = DEVICE_DT_GET(TEST_DEV_MULTIPLE_CALL);
+	enum pm_device_state state;
+	int ret;
+
+	/* Init domain */
+	pm_device_init_suspended(domain);
+	pm_device_runtime_enable(domain);
+
+	/* Init device */
+	pm_device_init_suspended(dev);
+	pm_device_runtime_enable(dev);
+
+	/* At this point domain should be SUSPENDED */
+	pm_device_state_get(domain, &state);
+	zassert_equal(state, PM_DEVICE_STATE_SUSPENDED);
+
+	/* At this point device should be SUSPENDED */
+	pm_device_state_get(dev, &state);
+	zassert_equal(state, PM_DEVICE_STATE_SUSPENDED);
+
+	/* Get and put the device without PM enabled should not change the domain */
+	ret = pm_device_runtime_get(dev);
+	zassert_equal(ret, 0);
+	pm_device_state_get(domain, &state);
+	zassert_equal(state, PM_DEVICE_STATE_ACTIVE);
+
+	ret = pm_device_runtime_get(dev);
+	zassert_equal(ret, 0);
+	pm_device_state_get(domain, &state);
+	zassert_equal(state, PM_DEVICE_STATE_ACTIVE);
+
+	ret = pm_device_runtime_put(dev);
+	zassert_equal(ret, 0);
+	pm_device_state_get(domain, &state);
+	zassert_equal(state, PM_DEVICE_STATE_ACTIVE);
+
+	ret = pm_device_runtime_put(dev);
+	zassert_equal(ret, 0);
+	pm_device_state_get(domain, &state);
+	zassert_equal(state, PM_DEVICE_STATE_SUSPENDED);
+}
+
 ZTEST_SUITE(power_domain_1cpu, NULL, NULL, ztest_simple_1cpu_before,
 			ztest_simple_1cpu_after, NULL);