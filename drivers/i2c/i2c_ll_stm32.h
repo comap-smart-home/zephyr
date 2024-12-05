8b0247bd6fa1672181743698510e0bdf7353aa3a
Baptiste Cluzel <baptiste.clu@gmail.com>
2024-12-05 17:08:05 +0100

    driver: i2c_ll_stm32: pm fix without runtime

--- drivers/i2c/i2c_ll_stm32.h
+++ drivers/i2c/i2c_ll_stm32.h
@@ -56,6 +56,9 @@ struct i2c_stm32_data {
 #endif
 	struct k_sem bus_mutex;
 	uint32_t dev_config;
+#if defined(CONFIG_SOC_SERIES_STM32WLX)
+	uint32_t i2c_clock;
+#endif
 #if DT_HAS_COMPAT_STATUS_OKAY(st_stm32_i2c_v2)
 	/* Store the current timing structure set by runtime config */
 	struct i2c_config_timing current_timing;
